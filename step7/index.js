84bc0852c1df5fa22695c8dc7f49b0a1272cb9a0
aianman4823 <akitoaianman1@gmail.com>
2020-06-20 17:43:22 +0900

    add step7

---
+++ step7/index.js
@@ -0,0 +1,20 @@
+// 17は偶数か奇数か
+const n = 17;
+function EvenOrOdd(n) {
+  // TODO: 偶数奇数判定関数を作成する
+}
+
+// 17は素数か素数でないか
+function is_prime(n) {
+  // TODO: 素数判定関数を作成する
+}
+
+
+
+
+// コラム
+// arrow関数という書き方がjsではよく使われる
+// 上記のEvenOrOdd関数の例をarrow関数に書き換えると
+// const EvenOrOdd = (n) => {}
+// という形で書くことができる
+// 今後よくみる形になるのでここで覚えておく
