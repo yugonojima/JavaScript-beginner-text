38455d2b223954576f0027af93e22b335157a5d5
aianman4823 <akitoaianman1@gmail.com>
2020-06-20 18:39:41 +0900

    add step9

---
+++ step9/index.js
@@ -0,0 +1,14 @@
+// TODO: <div id='root'><p id='count'>0</p></div>の中に
+// button要素(名前はボタンで良い)を追加し、
+// クリックすると1カウントが1進むようにする
+// ヒント: 文字列の数字を足し算することはできないので一度数値型に変える必要がある
+
+const root = document.getElementById("root");
+const count = document.getElementById("count");
+
+const handleClick = () => {};
+
+// TODO: 表示されている数字の上にボタンを追加したいときはどうするのかを調べる
+// ヒント
+//  https://developer.mozilla.org/ja/docs/Web/API/Element/insertAdjacentHTML
+//  https://qiita.com/butakoma/items/89fa687ab90ff28d57ef
